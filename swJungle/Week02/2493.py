104a0419689f783f9fd63548f4d63a947d6c7f1b
Gn0lee <jinho605@gmail.com>
2021-11-13 22:35:23 +0900

    2493 로테이트 방식 시간초과

---
+++ swJungle/Week02/2493.py
@@ -0,0 +1,34 @@
+from collections import deque
+
+tower_num = int(input())
+towers = list(map(int,input().split()))
+towers = deque(towers)
+ans= [0]*tower_num
+
+cnt = tower_num - 1
+
+while cnt > 0:
+    current_tower = towers[-1]
+    towers.rotate(1)
+    for i in range(1,cnt+1):
+        if i == cnt:
+            towers.rotate(1)
+            if towers[-1] > current_tower:
+                ans[cnt] = cnt-i
+                towers.rotate(-i)
+                break
+            else:
+                towers.rotate(-i)
+        elif i == 1:
+            if towers[-1] > current_tower:
+                ans[cnt] = cnt
+                break
+            towers.rotate(1)
+            if towers[-1] > current_tower:
+                ans[cnt] = cnt-i
+                towers.rotate(-i)
+                break
+    
+    
+    cnt -= 1
+print(*ans)
