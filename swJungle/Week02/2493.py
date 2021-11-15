4ce6b8e134dc96d0ffc7a570a12d038f4e2a59fd
Gn0lee <jinho605@gmail.com>
2021-11-15 23:51:00 +0900

    시간초과

--- swJungle/Week02/2493.py
+++ swJungle/Week02/2493.py
@@ -2,34 +2,22 @@ from collections import deque
 
 tower_num = int(input())
 towers = list(map(int,input().split()))
-towers = deque(towers)
+
 ans= [0]*tower_num
+stack = []
 
-cnt = tower_num - 1
 
-while cnt > 0:
-    current_tower = towers[-1]
-    towers.appendleft(towers.pop())
-
-    for i in range(1,cnt+1):
-        if i == cnt:
-            towers.appendleft(towers.pop())
-            if towers[-1] > current_tower:
-                ans[cnt] = cnt-i
-                towers.rotate(-i)
-                break
-            else:
-                towers.rotate(-i)
-        elif i == 1:
-            if towers[-1] > current_tower:
-                ans[cnt] = cnt
-                break
-            towers.appendleft(towers.pop())
-            if towers[-1] > current_tower:
-                ans[cnt] = cnt-i
-                towers.rotate(-i)
-                break
+for i,h in reversed(list(enumerate(towers))):
     
-    
-    cnt -= 1
+    search = i -1
+    while search >= 0:
+        if towers[search] > h:
+            break
+        else:
+            search -= 1
+
+    ans[i] = search + 1
+
+
+
 print(*ans)
