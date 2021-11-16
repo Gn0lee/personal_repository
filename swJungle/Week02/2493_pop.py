fa5a17daf66171ea2cf0569fa3bc7197a449ed30
Gn0lee <jinho605@gmail.com>
2021-11-16 21:25:07 +0900

    .

--- swJungle/Week02/2493_pop.py
+++ swJungle/Week02/2493_pop.py
@@ -1,21 +1,22 @@
 from collections import deque
 import sys
-from bisect import bisect_right,bisect_left
-tower_num = int(input())
-towers = list(map(int,sys.stdin.readline().split()))
-towers = deque(towers)
-ans= [0]*tower_num
 
-index = tower_num
+def searchTower(arr,n):
+    result = [0] * n
+    stack = []
+    for i in range(n-1,-1,-1):
+        stack.append([i,arr.pop()])
+        while stack and arr and arr[-1] >= stack[-1][1]:
+            idx , _ = stack.pop()
+            result[idx] = i
+    return result
 
-while towers:
-    current_tower = towers.pop()
-    
-    for i in range(index-2,-1,-1):
-        if current_tower < towers[i]:
-            ans[index-1] = i+1
-            break
 
-    index -= 1
 
-print(*ans)
+
+if __name__ == "__main__":
+
+    tower_num = int(input())
+    towers = list(map(int,sys.stdin.readline().split()))
+
+    print(*searchTower(towers,tower_num))
