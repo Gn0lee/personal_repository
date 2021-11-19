40eccd919410e1dc13c1822fb12877730b24146a
Gn0lee <jinho605@gmail.com>
2021-11-19 20:55:07 +0900

    .

--- swJungle/Week03/2606.py
+++ swJungle/Week03/2606.py
@@ -1,41 +1,39 @@
 import sys
 from collections import Counter
-input = sys.stdin.readline 
+input = sys.stdin.readline
 
 def find_parent(parent,x):
-
     if parent[x] != x:
         parent[x] = find_parent(parent,parent[x])
     return parent[x]
-        
 
 def union_parent(parent,a,b):
+
     a = find_parent(parent,a)
     b = find_parent(parent,b)
 
-    if a < b:
+    if a <b:
         parent[b] = a
     else:
         parent[a] = b
 
 
+computers = int(input())
+lines = int(input())
 
-n,m = map(int,input().split())
+parent = [0] * (computers+1)
 
-parent = [0]*(n+1)
-
-for i in range(1,n+1):
+for i in range(1,computers+1):
     parent[i] = i
 
-for _ in range(m):
-    a , b = map(int,input().split())
+for _ in range(lines):
+    a , b = map(int, input().split())
     union_parent(parent,a,b)
-    
-for i in range(1,n+1):
+
+for i in range(1,computers+1):
     find_parent(parent,i)
 
-
 # print(parent)
-# print(set(parent))
-# print(len(set(parent))-1)
-print(len(Counter(parent))-1)
+
+print(Counter(parent)[1]-1)
+print(len(Counter(parent)))
