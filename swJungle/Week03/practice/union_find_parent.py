a731325a2bf19673f77bda214ab86e6a745d77c0
Gn0lee <jinho605@gmail.com>
2021-11-19 16:54:53 +0900

    .

---
+++ swJungle/Week03/practice/union_find_parent.py
@@ -0,0 +1,28 @@
+def find_parent(parent,x): #노드의 루트를 찾는 함수
+    
+    #루트 노드를 찾을때 까지 재귀호출
+    if parent[x] != x:
+        return find_parent(parent,parent[x])
+    return x
+
+def union_parent(parent,a,b):
+
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+
+    if b >a : #루트가 작은쪽으로 루트를 갱신해줌
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+v , e = int(input().split())
+parent = [0] * (v+1)
+
+for i in range(1,v+1):
+    parent[i] = i
+
+for i in range(e):
+    a , b = map(int, input().split())
+
+    
