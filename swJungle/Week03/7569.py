c48f881c3e84f80aa0a326cb23cc8bdcf254a1c3
Gn0lee <jinho605@gmail.com>
2021-11-22 10:50:33 +0900

    .

--- swJungle/Week03/7569.py
+++ swJungle/Week03/7569.py
@@ -1,35 +1,49 @@
+from functools import reduce
 import sys
-from heapq import heappush,heappop
+from collections import deque
 input = sys.stdin.readline
 
 m , n ,h = map(int,input().split())
 
-tomatos = [[[] for _ in range(n)] for x in range(h)]
+tomatos = [[[0]*m for _ in range(n)] for x in range(h)]
 
-
+q = deque([])
 for i in range(h):
     for j in range(n):
         x = list(map(int,input().split()))
-        tomatos[i][j] = x
+        for index,k in enumerate(x):
+            if k == 1:
+                q.append([i,j,index])
+                tomatos[i][j][index] = k
+            else:
+                tomatos[i][j][index] = k
 
-dy = [-1,0,1,0]
-dx = [0,1,0,-1]
-dh = [1,-1]
+dy = [-1,0,1,0,0,0]
+dx = [0,1,0,-1,0,0]
+dh = [0,0,0,0,1,-1]
 
-def bfs(y,x,h):
-    
-    q = []
 
-    q.append([y,x,h])
+while q:
+    ch,cy,cx = q.popleft()
 
-    while q:
-        cy , cx ,ch = heappop(q)
+    for i in range(6):
+        ny = cy + dy[i]
+        nx = cx + dx[i]
+        nh = ch + dh[i]
 
-        for j in range(2):
-            for i in range(4):
-                ny = cy + dy[i]
-                nx = cx + dx[i]
-                nh = ch + dh[i]
+        if 0<=nh<h and 0<=nx<m and 0<=ny<n and tomatos[nh][ny][nx] == 0:
+            tomatos[nh][ny][nx] = tomatos[ch][cy][cx] + 1
+            q.append([nh,ny,nx])
 
-                if 0<=nh<h and 0<=nx<m and 0<=ny<n and tomatos[nh][ny][nx] == 1:
-                    heappush
+def time_cal():
+    max_time = int(-1e9)
+    for z in tomatos:
+        for y in z:
+            x = reduce(lambda a, b : a*b,y)
+            if x == 0:
+                return -1
+            else:
+                max_time = max(max_time,max(y))
+    return max_time-1
+
+print(time_cal())
