8f0d427992f9942e1aa2c4ced3082d4c6152280a
Gn0lee <jinho605@gmail.com>
2021-11-25 21:45:07 +0900

    .

--- swJungle/Week04/02.lecture_example/DP_example2.py
+++ swJungle/Week04/02.lecture_example/DP_example2.py
@@ -8,11 +8,11 @@ input = sys.stdin.readline
 
 x = int(input())
 
-dp = [0] *10000
+dp = [0] *30001
 
-dp[0] = 1
 
-for i in range(x+1):
+
+for i in range(2,x+1):
     dp[i] = dp[i-1] + 1
     if i%2 == 0:
         dp[i] = min(dp[i],dp[i//2]+1)
