a5e9aaab7d6e657042e26c3aac560cb13b2d58d7
Gn0lee <jinho605@gmail.com>
2021-11-26 10:42:56 +0900

    .

---
+++ swJungle/Week04/01.Assignment/9084.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    n = int(input())
+    money = list(map(int,input().split()))
+    m = int(input())
+    dp = [0] *(m+1)
+    dp[0] = 1
+
+    for i in money:
+        for j in range(i,m+1):
+            dp[j] += dp[j-i]
+
+    print(dp[m])
+    
