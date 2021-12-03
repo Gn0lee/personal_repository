5fb1eaa6743300919c722030adf1330793e665dd
Gn0lee <jinho605@gmail.com>
2021-12-03 13:56:58 +0900

    .

---
+++ swJungle/Week05/prac_c/variable/size.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void){
+
+    int x = 50;
+    float y = 12123124132411.12345;
+    double z = 13456345784567345634521.127909;
+    printf("x = %d\n",x);
+    printf("y = %.2f\n",y);
+    printf("z = %.2f\n",z);
+
+    return 0;
+}
