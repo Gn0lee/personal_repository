32b5ec29796ac0057035be10e1c7c672a6123f1f
Gn0lee <jinho605@gmail.com>
2021-12-08 20:43:10 +0900

    .

--- swJungle/Week05/algorithm_study/day3_42747.py
+++ swJungle/Week05/algorithm_study/day3_42747.py
@@ -1,14 +1,10 @@
-citations = [23, 1, 25, 21, 34, 21]
+def solution(citations):
+    answer = 0
+    
+    citations.sort(reverse=True)
+    
+    for i , h  in enumerate(citations):
+        if i+1 <= h:
+            answer = max(answer,i+1)
 
-
-answer = 0
-citation_list = [0]*10001
-
-for i in citations:
-    citation_list[i] += 1
-
-for j in citations:
-    if sum(citation_list[j:]) >= j and sum(citation_list[:j+1])<=j: 
-        answer = max(answer,j)
-
-print(answer)
+    return answer
