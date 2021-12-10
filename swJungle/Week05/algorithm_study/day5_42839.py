bc4a3e7b1fe616d3da20870a4b39140f4e3154a1
Gn0lee <jinho605@gmail.com>
2021-12-10 13:39:24 +0900

    .

--- swJungle/Week05/algorithm_study/day5_42839.py
+++ swJungle/Week05/algorithm_study/day5_42839.py
@@ -3,6 +3,7 @@ def solution(numbers):
     from itertools import permutations
     from math import sqrt
     
+    # 소수 확인하는 함수
     def chk_prime_num(x):
         if x == 0 or x == 1:
             return False
@@ -11,19 +12,20 @@ def solution(numbers):
                 return False
         return True
     
-    l = set()
+    permu_nums = set()
     
+    # 순열로 조합하여 가능한 경우를 permu_nums에 add
     for i in range(1,8):        
         for j in permutations(numbers,i):
             if j[0] == '0':
                 continue
-            permu_num =''
+            permu_str =''
             for k in j:
-                permu_num += str(k)
-                l.add(int(permu_num))
+                permu_str += str(k)
+                permu_nums.add(int(permu_str))
     
-    for x in l:
-        if chk_prime_num(x):
+    for permu_num in permu_nums:
+        if chk_prime_num(permu_num):
             answer+=1
     
     return answer
