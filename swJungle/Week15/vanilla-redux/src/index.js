4391d90899d6c1059e798c2e0649798f933f5d59
Gn0lee <jinho605@gmail.com>
2022-02-18 14:09:09 +0900

    .

--- swJungle/Week15/vanilla-redux/src/index.js
+++ swJungle/Week15/vanilla-redux/src/index.js
@@ -1,22 +1,31 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.querySelector("span");
+number.innerText = 0;
 
-let count = 0;
-number.innerText = count;
+const countModifier = (count = 0, action) =>{
+  // console.log(count , action);
+  if(action.type === "ADD"){
+   return count + 1; 
+  }else if (action.type === "MINUS"){
+    return count - 1;
+  }else{
+    return count;
+  }
+  //  console.log(action);
+};
 
-const updateText = () => {
-  number.innerText = count;
+const countStore = createStore(countModifier);
+
+const onChage = () => {
+  // console.log(countStore.getState())
+  number.innerText = countStore.getState();
 }
 
-const handleAdd = ()=>{
-  count = count + 1;
-  updateText();
-};
-const handleMinus = ()=>{
-  count = count -1 ;
-  updateText();
-};
+countStore.subscribe(onChage);
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
+add.addEventListener("click", ()=>countStore.dispatch({type : "ADD"}));
+minus.addEventListener("click", ()=>countStore.dispatch({type : "MINUS"}));
+
