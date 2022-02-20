cfa95e68c032ed69b9690cff70e4dceeadc5d840
Gn0lee <jinho605@gmail.com>
2022-02-21 03:26:39 +0900

    .

--- swJungle/Week15/vanilla-redux/src/store.js
+++ swJungle/Week15/vanilla-redux/src/store.js
@@ -1,37 +1,45 @@
 import {createStore} from "redux";
+import {configureStore, createAction, createReducer, createSlice} from "@reduxjs/toolkit";
 
-const ADD = "ADD";
-const DELETE = "DELETE";
 
-const addToDo = (text) => {
-    return{
-        type : ADD,
-        text
+// const addToDo = createAction("ADD");
+// const deleteToDo = createAction("DELETE")
+
+// const reducer = (state = [], action) => {
+//     switch(action.type){
+//         case addToDo.type:
+//             return[{text : action.payload , id: Date.now()}, ...state]
+//         case deleteToDo.type:
+//             return state.filter((toDo) => toDo.id !== action.payload);
+//         default:
+//             return state;
+//     }
+// }
+
+// const reducer = createReducer([],{
+//     [addToDo] : (state , action) => {
+//         state.push({text : action.payload , id: Date.now()});
+//     },
+//     [deleteToDo] : (state , action) => 
+//         state.filter(toDo => toDo.id !== action.payload)
+    
+// });
+
+const toDos = createSlice({
+    name : "todoReducer",
+    initialState : [],
+    reducers : {
+        add : (state , action) => {
+            state.push({text : action.payload , id: Date.now()});
+        },
+        remove : (state , action) => state.filter(toDo => toDo.id !== action.payload)
     }
-}
-const deleteToDo = (id) => {
-    return{
-        type : DELETE,
-        id : parseInt(id),
-    }
-}
+});
 
-const reducer = (state = [], action) => {
-    switch(action.type){
-        case ADD:
-            return[{text : action.text , id: Date.now()}, ...state]
-        case DELETE:
-            return state.filter((toDo) => toDo.id !== action.id);
-        default:
-            return state;
-    }
-}
 
-const store = createStore(reducer);
 
-export const actionCreators = {
-    addToDo,
-    deleteToDo
-}
+const store = configureStore({reducer : toDos.reducer});
+
+export const {add , remove} = toDos.actions;
 
 export default store;
