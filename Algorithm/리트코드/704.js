fceea64690f9c46b2a28a9289321f9c425d01e69
Gn0lee <jinho605@gmail.com>
2022-04-25 20:25:45 +0900

    .

---
+++ Algorithm/리트코드/704.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ var search = function(nums, target) {
+    let start = 0;
+    let end = nums.length;
+    
+    while(start <= end){
+        let mid = Math.floor((start + end) / 2);
+        
+        if(nums[mid] === target){
+            return mid;
+        }else if(nums[mid] > target){
+            end = mid - 1;
+        }else{
+            start = mid + 1;
+        }
+    }
+    return -1;
+};
