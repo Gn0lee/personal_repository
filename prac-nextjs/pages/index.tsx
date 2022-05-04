f31b669364c8e9d4bf6a65d280f048b3675d17bc
Gn0lee <jinho605@gmail.com>
2022-05-04 20:50:38 +0900

    .

--- prac-nextjs/pages/index.tsx
+++ prac-nextjs/pages/index.tsx
@@ -7,11 +7,7 @@ export default function Home() {
   const [movies, setMovies] = useState<any>();
   useEffect(() => {
     (async () => {
-      const { results } = await (
-        await fetch(
-          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
-        )
-      ).json();
+      const { results } = await (await fetch("/api/movies")).json();
       setMovies(results);
     })();
   }, []);
@@ -20,7 +16,10 @@ export default function Home() {
       <SEO title="Home" />
       {!movies && <h4>Loading..</h4>}
       {movies?.map((movie: any) => (
-        <h4>{movie.original_title}</h4>
+        <div className="movie" key={movie.id}>
+          <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} />
+          <h4>{movie.original_title}</h4>
+        </div>
       ))}
       <style jsx>{`
         .container {
